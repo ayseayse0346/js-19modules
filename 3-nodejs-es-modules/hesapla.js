aa3c66a912c094d85f2df2da14edcd723c16e0ce
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-03-15 21:35:10 +0300

    add

--- 3-nodejs-es-modules/hesapla.js
+++ 3-nodejs-es-modules/hesapla.js
@@ -5,3 +5,7 @@ export const topla = (a, b) => a + b
 //? CommonJS'nin ve ES-Modülün birbirlerine göre  avantaj ve dezavantajları vardır.
 //? Ancak Backend uygulamaları için geliştirilen birçok eski modül hala Common JS modül yapisini kullandığı için yaygın olan kullanım Common JS'dir.
 //? Front-end uygulamaları için ES-Modül yapisi kullanılmaktadir.
+
+//! node.js ortamında DOM API yoktur. (Window)
+//  prompt("Bir sayi girniz")
+// const h1 = document.getElementById("h1") 

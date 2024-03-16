7ec3a5d384c14c07193346dde66106cfde33caea
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-03-16 12:03:34 +0300

    add

--- 2-browser-es-modules/app.js
+++ 2-browser-es-modules/app.js
@@ -3,7 +3,7 @@
 //*************************************/
 //? ES Modules, EcmaScript 2015 versiyonu ile gelmiştir.
 //? Günümüzde birçok browser ES modül yapısını desteklemektedir.
-//? JS kodlarının kolay ve performansli bir şekilde farklı dosyalara ayrilmasina ve gerektiginde birleştirmesine olanak saglamaktadir.
+//? JS kodlarının kolay ve performansli bir şekilde farklı dosyalara ayrilmasina ve gerektiginde birleştirmesine olanak saglamaktadir. 
 //? Bir degisken,dizi,obje, fonksiyon, class v.b "export" deyimi ile başka dosyalarinin kullanimina açilmabilir.
 //? Kullanima acilanlar ise import deyimi ile istenilen dosyaya eklenebilir.
 
